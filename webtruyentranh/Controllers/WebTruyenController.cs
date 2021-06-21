e96bf0c1fedf8b7b18129f003eecdf52d08b73f6
Phuc Tu <tangtuphuc2017@gmail.com>
2021-06-21 23:41:08 +0700

    Phuc

---
+++ webtruyentranh/Controllers/WebTruyenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webtruyentranh.Controllers
+{
+    public class WebTruyenController : Controller
+    {
+        // GET: WebTruyen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
